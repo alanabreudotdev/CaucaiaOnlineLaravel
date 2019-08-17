e587c82b02e5cc414f23028fb938894a0a46e848
Alan Abreu <L3AN@MacBook-Pro-de-Alan.local>
2019-08-17 16:21:56 -0300

    Apoio

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,12 @@
 |
 */
 
+/*
 Route::get('/', function(){
   echo 'ok!';
 });
-//Route::get('/', 'HomeController@index')->name('home');
+*/
+Route::get('/', 'HomeController@index')->name('home');
 
 //ROTAS RECLAMAÇÃO
 Route::get('/reclamar', 'Front\\ReclamarController@index')->name('reclamacao.front.index');
