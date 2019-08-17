037613aed07f77eeb0b43f608805c42fb94ffb78
Alan Abreu <L3AN@MacBook-Pro-de-Alan.local>
2019-08-17 14:11:35 -0300

    Apoio

--- app/Http/Controllers/API/v1/IndexController.php
+++ app/Http/Controllers/API/v1/IndexController.php
@@ -132,14 +132,13 @@ class IndexController extends Controller
     }
 
     public function userUpdate(Request $request){
-      $request->birthday = str_replace($request->birthday, '', '-');
 
       if(strlen($request->birthday)==10){
         $date = date_create_from_format("d/m/Y", $request->birthday)->format("Y-m-d");
       }else{
         return response()->json([
           'success'=>false,
-          'message'=> $request->birthday
+          'message'=> 'Data invalida.\n Digite corretamente.\n Ex: 10/06/1990'
         ]);
       }
 
