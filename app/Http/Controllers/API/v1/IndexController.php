368a31d720d986823657381c07f973c1944e209d
Alan Abreu <L3AN@MacBook-Pro-de-Alan.local>
2019-08-17 14:16:44 -0300

    Apoio

--- app/Http/Controllers/API/v1/IndexController.php
+++ app/Http/Controllers/API/v1/IndexController.php
@@ -133,7 +133,7 @@ class IndexController extends Controller
 
     public function userUpdate(Request $request){
       $request->birthday = str_replace($request->birthday, '', '-');
-      print_r($request->birthday);
+      
       if(strlen($request->birthday)==10){
         $date = date_create_from_format("d/m/Y", $request->birthday)->format("Y-m-d");
       }else{
