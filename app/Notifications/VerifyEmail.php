f065dd4127c3cf35deee6cd36bff3583e16b40c1
Alan Abreu <L3AN@MacBook-Pro-de-Alan.local>
2019-08-18 11:59:59 -0300

    Email Reset e validation

--- app/Notifications/VerifyEmail.php
+++ app/Notifications/VerifyEmail.php
@@ -41,9 +41,9 @@ class VerifyEmail extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+                    ->subject('Valide seu email')
+                    ->action('Clique aqui para validar', $this->verificationUrl($notifiable))
+                    ->line('Se você não criou uma conta, nenhuma outra ação será necessária.');
     }
 
     /**
