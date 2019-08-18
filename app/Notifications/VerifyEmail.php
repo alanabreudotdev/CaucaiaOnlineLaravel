3bf0d72e14aa0a8dc27cc717467feb805dcc98ae
Alan Abreu <L3AN@MacBook-Pro-de-Alan.local>
2019-08-18 12:02:33 -0300

    Email Reset e validation

--- app/Notifications/VerifyEmail.php
+++ app/Notifications/VerifyEmail.php
@@ -61,4 +61,17 @@ class VerifyEmail extends Notification
             //
         ];
     }
+
+    /**
+     * Get the verification URL for the given notifiable.
+     *
+     * @param  mixed  $notifiable
+     * @return string
+     */
+    protected function verificationUrl($notifiable)
+    {
+        return URL::temporarySignedRoute(
+            'verification.verify', Carbon::now()->addMinutes(60), ['id' => $notifiable->getKey()]
+        );
+    }
 }
