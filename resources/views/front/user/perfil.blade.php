b3f23d6ba63f709dbd5ccbac6c08301afb567ca1
Alan Abreu <L3AN@MacBook-Pro-de-Alan.local>
2019-08-19 12:31:53 -0300

    Privacidade

--- resources/views/front/user/perfil.blade.php
+++ resources/views/front/user/perfil.blade.php
@@ -20,16 +20,16 @@
                                     <div class="card card-profile-feed">
                                         <div class="card-header card-header-action">
                                             <div class="media align-items-center">
-                                              
+
                                                 <div class="media-body">
                                                     <h3 class="text-capitalize font-weight-500 text-dark">Perfil</h3>
                                                     <div class="font-13">Adicione informações sobre você</div>
                                                 </div>
                                             </div>
-                                            
+
                                         </div>
                                         <div class="card-body">
-                                             
+
                                         <div class="row">
                                         <div class="col-sm">
                                         <form method="POST" action="{{ route('usuario.perfil.post') }}">
@@ -42,7 +42,7 @@
                                                             <div class="alert alert-danger">{{ $message }}</div>
                                                         @enderror
                                                     </div>
-                                                    
+
                                                     <div class="col-md-6 form-group">
                                                         <label for="lastName">Sobrenome</label>
                                                     <input class="form-control" value="{{ Auth::user()->lastname }}" id="lastname" name="lastname" placeholder="" value="" type="text">
@@ -57,7 +57,7 @@
                                                                 <div class="alert alert-danger">{{ $message }}</div>
                                                             @enderror
                                                         </div>
-                                                        
+
                                                         <div class="col-md-6 form-group">
                                                             <label for="rg">RG</label>
                                                         <input class="form-control" value="{{ Auth::user()->rg }}" id="rg" name="rg" placeholder="" value="" type="tel">
@@ -70,15 +70,15 @@
                                                 </div>
                                                 <hr>
 
-                                                <!--PRIVACIDADE -->
+                                                <!--PRIVACIDADE
                                                 <h3 class="text-capitalize font-weight-500 text-dark">Privacidade</h3>
                                                 <div class="row mb-25">
-                                                    
+
                                                     <div class="col-md-6 mt-15">
                                                         <div class="custom-control custom-radio">
                                                             <input type="radio" id="reclamacao_privacidade1" value="1" @if(Auth::user()->reclamacao_privacidade == 1) checked @endif name="reclamacao_privacidade" class="custom-control-input">
                                                             <label class="custom-control-label" for="reclamacao_privacidade1">Público</label>
-                                                            
+
                                                         </div>
                                                         <small>Seu nome será mostrado quando você efetuar uma reclamação</small>
                                                     </div>
@@ -86,30 +86,30 @@
                                                         <div class="custom-control custom-radio">
                                                             <input type="radio" id="reclamacao_privacidade2" value="0" name="reclamacao_privacidade" @if(Auth::user()->reclamacao_privacidade == 0) checked @endif class="custom-control-input">
                                                             <label class="custom-control-label" for="reclamacao_privacidade2">Privado</label>
-                                                            
+
                                                         </div>
                                                         <small>Seu nome NÃO será mostrado quando você efetuar uma reclamação</small>
                                                     </div>
                                                 </div>
                                                 <small class="align-center font-13">Nos comentários postados em reclamações, notícias e nos tópicos das comunidades o nome do usuário, por questões legais, será sempre exibido.</small>
-
+                                                -->
                                                 <!--FIM PRIVACIDADE -->
                                                 <hr>
-                                                
+
                                                 <div class="text-center">
                                                         <button class="btn btn-primary text" type="submit">Salvar</button>
-                                                </div>                                            
+                                                </div>
                                             </form>
                                         </div>
                                          </div>
                                         </div>
-                                        
+
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>	
+                </div>
             </div>
         </div>
         <!-- /Row -->
@@ -117,5 +117,5 @@
     <!-- /Container -->
 @endsection
 @section('js_after')
-    
+
 @endsection
